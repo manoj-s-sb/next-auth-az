2944813e03445e77d6d4a68c0030138213b047c7
Manojkumar <manojkumar@Manojkumar-ka-MacBook-Air.local>
2025-01-17 15:39:16 +0530

    client-msal

--- my-app/app/utils/msalConfig.ts
+++ my-app/app/utils/msalConfig.ts
@@ -1,47 +1,5 @@
 import { LogLevel, PublicClientApplication } from "@azure/msal-browser";
 
-export const msalConfig = {
-  auth: {
-    clientId: "d710cec4-128a-47c2-8cbe-7c37852eccaa",
-    authority:
-      "https://stancebeamcctest.b2clogin.com/stancebeamcctest.onmicrosoft.com/B2C_1_SIgnUp",
-    knownAuthorities: ["stancebeamcctest.b2clogin.com"],
-    redirectUri: "http://localhost:3000/",
-    postLogoutRedirectUri: "http://localhost:3000/",
-    navigateToLoginRequestUrl: false,
-  },
-  cache: {
-    cacheLocation: "localStorage",
-    storeAuthStateInCookie: true,
-  },
-
-  system: {
-    loggerOptions: {
-      loggerCallback: (level:LogLevel, message:string, containsPii:boolean) => {
-        if (containsPii) {
-          return;
-        }
-        switch (level) {
-          case LogLevel.Error:
-            console.error(message);
-            return;
-          case LogLevel.Info:
-            console.info(message);
-            return;
-          case LogLevel.Verbose:
-            console.debug(message);
-            return;
-          case LogLevel.Warning:
-            console.warn(message);
-            return;
-          default:
-            return;
-        }
-      },
-    },
-  },
-};
-
 export const b2cPolicies = {
   signUp: {
     authority:
@@ -61,10 +19,35 @@ export const b2cPolicies = {
   },
 };
 
+export const msalConfig = {
+  auth: {
+    clientId: "d710cec4-128a-47c2-8cbe-7c37852eccaa",
+    authority:
+      "https://stancebeamcctest.b2clogin.com/stancebeamcctest.onmicrosoft.com/B2C_1_SIgnUp",
+    knownAuthorities: ["stancebeamcctest.b2clogin.com"],
+    //redirectUri: "http://localhost:3000/",
+  },
+  cache: {
+    cacheLocation: "localStorage",
+    storeAuthStateInCookie: true,
+  },
+
+  system: {
+    loggerOptions: {
+      loggerCallback: (level: LogLevel, message: string) => {
+        if (process.env.NODE_ENV !== "production") {
+          console.log(message);
+        }
+      },
+      logLevel: LogLevel.Error,
+    },
+  },
+};
+
 export const msalInstance = new PublicClientApplication(msalConfig);
 
 export const loginRequest = {
-    scopes: [
-        "https://stancebeamcctest.onmicrosoft.com/2e2ab414-7175-411b-8a5d-d11b591cbd07/api.read",
-      ],
-  };
+  scopes: [
+    "https://stancebeamcctest.onmicrosoft.com/2e2ab414-7175-411b-8a5d-d11b591cbd07/api.read",
+  ],
+};
